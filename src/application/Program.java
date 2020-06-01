f9a5851a5494f68a8258049f2f7715dd671ad048
Vitor Jardim <vitorribeiroj@gmail.com>
2020-06-01 15:59:26 -0300

    ChessException and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import Boardgame.Board;
 import Chess.ChessMatch;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		UI.printBoard(chessMatch.getPieces()); 
 		
 	}
 
