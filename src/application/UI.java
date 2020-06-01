b283aab86377188ec22ae1952eaa3ddd233316fb
Vitor Jardim <vitorribeiroj@gmail.com>
2020-06-01 16:45:18 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import Chess.ChessPiece;
+import Chess.ChessPosition;
 import Chess.Color;
 
 public class UI {
@@ -23,6 +27,20 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		
+		try {
+		String s = sc.nextLine();
+		char column = s.charAt(0);
+		int row = Integer.parseInt(s.substring(1));
+		return new ChessPosition(column, row);
+		}
+		catch(RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition.");
+		}
+	}
 	
 	
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -53,5 +71,8 @@ public class UI {
         }
         System.out.print(" ");
 	}
+
+	
+	
 	
 }
